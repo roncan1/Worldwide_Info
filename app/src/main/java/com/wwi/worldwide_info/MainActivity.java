0ec35264ae1427cb92d9022cc1fa42e90808de2a
roncan <65841016+roncan1@users.noreply.github.com>
2021-10-07 20:57:50 +0900

    전체화면기능 추가

--- app/src/main/java/com/wwi/worldwide_info/MainActivity.java
+++ app/src/main/java/com/wwi/worldwide_info/MainActivity.java
@@ -3,6 +3,7 @@ package com.wwi.worldwide_info;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +11,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        doFullScreen();
         setContentView(R.layout.activity_main);
 
     }
 
+    private void doFullScreen() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|
+                        View.SYSTEM_UI_FLAG_FULLSCREEN|
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION );
+    }
 
 }
