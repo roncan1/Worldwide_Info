7d5ca97ce0d82803534558f09d84d457910f42ea
roncan <65841016+roncan1@users.noreply.github.com>
2021-10-09 04:30:50 +0900

    인포화면 전환과 애니매이션 추가 #2

--- app/src/main/java/com/wwi/worldwide_info/InfoActivity.java
+++ app/src/main/java/com/wwi/worldwide_info/InfoActivity.java
@@ -1,18 +1,50 @@
 package com.wwi.worldwide_info;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import tyrantgit.explosionfield.ExplosionField;
 
 public class InfoActivity extends AppCompatActivity {
+
+    ExplosionField explosionField;
+    ImageButton btn_end_info;
+    ConstraintLayout thisActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.vertical_in, R.anim.none);
         setContentView(R.layout.activity_info);
+        init();
         doFullScreen();
+        endInfoActivity();
+
+
+    }
+
+
+    void endInfoActivity() {
+        btn_end_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                explosionField.explode(btn_end_info);
+                explosionField.explode(thisActivity);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                    }
+                }, 400);
+            }
+        });
     }
 
     private void doFullScreen() {
@@ -21,5 +53,12 @@ public class InfoActivity extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|
                         View.SYSTEM_UI_FLAG_FULLSCREEN|
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION );
+    }
+
+
+    public void init() {
+        btn_end_info = (ImageButton) findViewById(R.id.btn_end_info);
+        explosionField = ExplosionField.attach2Window(this);
+        thisActivity = (ConstraintLayout) findViewById(R.id.infoActivity);
     }
 }
--- app/src/main/java/com/wwi/worldwide_info/MainActivity.java
+++ app/src/main/java/com/wwi/worldwide_info/MainActivity.java
@@ -6,20 +6,24 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout background;
-    ImageButton[] country;
-    ImageButton openInfo;
+    ImageButton[] btn_country;
+    ImageButton btn_open_info;
     ImageView map;
     int infoNum = 0;
     Boolean[] checkCountry;
+    Animation info_open_anim;
 
-//    0 = 일본 | 1 = 한국 | 2 = 중국 | 3 = 인도 | 4 = 러시아
+    //    0 = 일본 | 1 = 한국 | 2 = 중국 | 3 = 인도 | 4 = 러시아
 //    5 = 이집트 | 6 = 이탈리아 | 7 = 프랑스 | 8 = 칠레 | 9 = 미국
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,26 +40,26 @@ public class MainActivity extends AppCompatActivity {
     private void doFullScreen() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|
-                        View.SYSTEM_UI_FLAG_FULLSCREEN|
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION );
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
     public void selectCountry() {
-        for (int i = 0; i < 10; i ++) {
+        for (int i = 0; i < 10; i++) {
             int nowNum = i;
-            country[i].setOnClickListener(new View.OnClickListener() {
+            btn_country[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     infoNum = nowNum;
                     if (checkCountry[nowNum] == false) {
                         checkCountry[nowNum] = true;
                         changeIcon(infoNum);
-                        openInfo.setVisibility(View.VISIBLE);
+                        btn_open_info.setVisibility(View.VISIBLE);
                     } else {
                         checkCountry[nowNum] = false;
                         resetIcon();
-                        openInfo.setVisibility(View.GONE);
+                        btn_open_info.setVisibility(View.GONE);
                     }
                 }
             });
@@ -66,29 +70,69 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 10; i++) {
             if (i == infoNum) {
                 switch (infoNum) {
-                    case 0: country[i].setImageResource(R.drawable.ic_col_japan);break;
-                    case 1: country[i].setImageResource(R.drawable.ic_col_korea);break;
-                    case 2: country[i].setImageResource(R.drawable.ic_col_china);break;
-                    case 3: country[i].setImageResource(R.drawable.ic_col_india);break;
-                    case 4: country[i].setImageResource(R.drawable.ic_col_russia);break;
-                    case 5: country[i].setImageResource(R.drawable.ic_col_egypt);break;
-                    case 6: country[i].setImageResource(R.drawable.ic_col_italia);break;
-                    case 7: country[i].setImageResource(R.drawable.ic_col_france);break;
-                    case 8: country[i].setImageResource(R.drawable.ic_col_chile);break;
-                    case 9: country[i].setImageResource(R.drawable.ic_col_usa);break;
+                    case 0:
+                        btn_country[i].setImageResource(R.drawable.ic_col_japan);
+                        break;
+                    case 1:
+                        btn_country[i].setImageResource(R.drawable.ic_col_korea);
+                        break;
+                    case 2:
+                        btn_country[i].setImageResource(R.drawable.ic_col_china);
+                        break;
+                    case 3:
+                        btn_country[i].setImageResource(R.drawable.ic_col_india);
+                        break;
+                    case 4:
+                        btn_country[i].setImageResource(R.drawable.ic_col_russia);
+                        break;
+                    case 5:
+                        btn_country[i].setImageResource(R.drawable.ic_col_egypt);
+                        break;
+                    case 6:
+                        btn_country[i].setImageResource(R.drawable.ic_col_italia);
+                        break;
+                    case 7:
+                        btn_country[i].setImageResource(R.drawable.ic_col_france);
+                        break;
+                    case 8:
+                        btn_country[i].setImageResource(R.drawable.ic_col_chile);
+                        break;
+                    case 9:
+                        btn_country[i].setImageResource(R.drawable.ic_col_usa);
+                        break;
                 }
             } else {
                 switch (i) {
-                    case 0: country[i].setImageResource(R.drawable.ic_blur_japan);break;
-                    case 1: country[i].setImageResource(R.drawable.ic_blur_korea);break;
-                    case 2: country[i].setImageResource(R.drawable.ic_blur_china);break;
-                    case 3: country[i].setImageResource(R.drawable.ic_blur_india);break;
-                    case 4: country[i].setImageResource(R.drawable.ic_blur_russia);break;
-                    case 5: country[i].setImageResource(R.drawable.ic_blur_egypt);break;
-                    case 6: country[i].setImageResource(R.drawable.ic_blur_italia);break;
-                    case 7: country[i].setImageResource(R.drawable.ic_blur_france);break;
-                    case 8: country[i].setImageResource(R.drawable.ic_blur_chile);break;
-                    case 9: country[i].setImageResource(R.drawable.ic_blur_usa);break;
+                    case 0:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_japan);
+                        break;
+                    case 1:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_korea);
+                        break;
+                    case 2:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_china);
+                        break;
+                    case 3:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_india);
+                        break;
+                    case 4:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_russia);
+                        break;
+                    case 5:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_egypt);
+                        break;
+                    case 6:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_italia);
+                        break;
+                    case 7:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_france);
+                        break;
+                    case 8:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_chile);
+                        break;
+                    case 9:
+                        btn_country[i].setImageResource(R.drawable.ic_blur_usa);
+                        break;
                 }
             }
         }
@@ -97,12 +141,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openInfo(int country) {
-        openInfo.setOnClickListener(new View.OnClickListener() {
+        btn_open_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
-                intent.putExtra("country", country);
-                startActivity(intent);
+                    btn_open_info.startAnimation(info_open_anim);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        btn_open_info.setImageResource(R.drawable.open_info2);
+                    }
+                }, 300);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                        intent.putExtra("country", country);
+                        startActivity(intent);
+                    }
+                }, 700);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        btn_open_info.setImageResource(R.drawable.open_info);
+                    }
+                }, 2000);
             }
         });
     }
@@ -110,16 +174,36 @@ public class MainActivity extends AppCompatActivity {
     public void resetIcon() {
         for (int i = 0; i < 10; i++) {
             switch (i) {
-                case 0: country[i].setImageResource(R.drawable.ic_def_japan);break;
-                case 1: country[i].setImageResource(R.drawable.ic_def_korea);break;
-                case 2: country[i].setImageResource(R.drawable.ic_def_china);break;
-                case 3: country[i].setImageResource(R.drawable.ic_def_india);break;
-                case 4: country[i].setImageResource(R.drawable.ic_def_russia);break;
-                case 5: country[i].setImageResource(R.drawable.ic_def_egypt);break;
-                case 6: country[i].setImageResource(R.drawable.ic_def_italia);break;
-                case 7: country[i].setImageResource(R.drawable.ic_def_france);break;
-                case 8: country[i].setImageResource(R.drawable.ic_def_chile);break;
-                case 9: country[i].setImageResource(R.drawable.ic_def_usa);break;
+                case 0:
+                    btn_country[i].setImageResource(R.drawable.ic_def_japan);
+                    break;
+                case 1:
+                    btn_country[i].setImageResource(R.drawable.ic_def_korea);
+                    break;
+                case 2:
+                    btn_country[i].setImageResource(R.drawable.ic_def_china);
+                    break;
+                case 3:
+                    btn_country[i].setImageResource(R.drawable.ic_def_india);
+                    break;
+                case 4:
+                    btn_country[i].setImageResource(R.drawable.ic_def_russia);
+                    break;
+                case 5:
+                    btn_country[i].setImageResource(R.drawable.ic_def_egypt);
+                    break;
+                case 6:
+                    btn_country[i].setImageResource(R.drawable.ic_def_italia);
+                    break;
+                case 7:
+                    btn_country[i].setImageResource(R.drawable.ic_def_france);
+                    break;
+                case 8:
+                    btn_country[i].setImageResource(R.drawable.ic_def_chile);
+                    break;
+                case 9:
+                    btn_country[i].setImageResource(R.drawable.ic_def_usa);
+                    break;
             }
         }
         map.setImageResource(R.drawable.map_demo);
@@ -127,21 +211,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void init() {
-        country = new ImageButton[10];
-        checkCountry = new Boolean[] {false, false, false, false, false, false, false, false, false, false};
+        btn_country = new ImageButton[10];
+        checkCountry = new Boolean[]{false, false, false, false, false, false, false, false, false, false};
         background = (ConstraintLayout) findViewById(R.id.background);
         map = (ImageView) findViewById(R.id.map);
-        openInfo = (ImageButton) findViewById(R.id.open_info);
-        country[0] = (ImageButton) findViewById(R.id.btn_japan);
-        country[1] = (ImageButton) findViewById(R.id.btn_korea);
-        country[2] = (ImageButton) findViewById(R.id.btn_china);
-        country[3] = (ImageButton) findViewById(R.id.btn_india);
-        country[4] = (ImageButton) findViewById(R.id.btn_russia);
-        country[5] = (ImageButton) findViewById(R.id.btn_egypt);
-        country[6] = (ImageButton) findViewById(R.id.btn_Italia);
-        country[7] = (ImageButton) findViewById(R.id.btn_france);
-        country[8] = (ImageButton) findViewById(R.id.btn_chile);
-        country[9] = (ImageButton) findViewById(R.id.btn_usa);
+        info_open_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.open_info);
+        btn_open_info = (ImageButton) findViewById(R.id.open_info);
+        btn_country[0] = (ImageButton) findViewById(R.id.btn_japan);
+        btn_country[1] = (ImageButton) findViewById(R.id.btn_korea);
+        btn_country[2] = (ImageButton) findViewById(R.id.btn_china);
+        btn_country[3] = (ImageButton) findViewById(R.id.btn_india);
+        btn_country[4] = (ImageButton) findViewById(R.id.btn_russia);
+        btn_country[5] = (ImageButton) findViewById(R.id.btn_egypt);
+        btn_country[6] = (ImageButton) findViewById(R.id.btn_Italia);
+        btn_country[7] = (ImageButton) findViewById(R.id.btn_france);
+        btn_country[8] = (ImageButton) findViewById(R.id.btn_chile);
+        btn_country[9] = (ImageButton) findViewById(R.id.btn_usa);
     }
 
 
